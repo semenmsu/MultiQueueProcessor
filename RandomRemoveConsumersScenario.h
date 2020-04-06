c7ca02b3638248c0220992167b787bec34e88c4e
Semen <semenmsu@gmail.com>
2020-04-06 17:27:43 +0300

    fix bag

--- RandomRemoveConsumersScenario.h
+++ RandomRemoveConsumersScenario.h
@@ -6,9 +6,9 @@
 #include "RandomRemoveConsumersBag.h"
 #include "ProducersBag.h"
 
-
-template<typename Value, typename Queue>
-int RandomRemoveConsumersTest(uint32_t consumers_num, uint32_t delay_msk, std::string queue_name) {
+template <typename Value, typename Queue>
+int RandomRemoveConsumersTest(uint32_t consumers_num, uint32_t delay_msk, std::string queue_name)
+{
 	MultiQueueProcessor<int32_t, Value, Queue> mqp;
 	ProducersBag<int32_t, Value> producers(mqp, consumers_num, 1);
 	RandomRemoveConsumersBag<int32_t, Value> consumers(mqp, consumers_num, delay_msk);
@@ -24,12 +24,12 @@ int RandomRemoveConsumersTest(uint32_t consumers_num, uint32_t delay_msk, std::s
 template <typename Value>
 void RandomRemoveConsumersScenario(uint32_t consumers_num, uint32_t delay_mks)
 {
-    std::cout << "================================================================" << std::endl;
-    std::cout << "RandomSubscribeUnsusbscribeScenario               " << std::endl;
-    std::cout << "Number Producers And Consumers " << consumers_num << std::endl;
-    std::cout << "SizeOfValue = " << sizeof(Value) << "                    " << std::endl
-        << std::endl;
+	std::cout << "================================================================" << std::endl;
+	std::cout << "RandomRemoveConsumersScenario               " << std::endl;
+	std::cout << "Number Producers And Consumers " << consumers_num << std::endl;
+	std::cout << "SizeOfValue = " << sizeof(Value) << "                    " << std::endl
+			  << std::endl;
 	RandomRemoveConsumersTest<int32_t, BruteForceQueue<int32_t, Value>>(consumers_num, delay_mks, "BruteForceQueue");
-    std::cout << "================================================================" << std::endl
-        << std::endl;
+	std::cout << "================================================================" << std::endl
+			  << std::endl;
 }
